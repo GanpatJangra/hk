d678c0bac970cd22f5207a693d033c594242b1d9
Ganpat Jangra <Ganpatjangra@gmail.com>
2024-10-12 01:25:34 +0530

    Update data_poller.py

--- src/pipeline/data_poller.py
+++ src/pipeline/data_poller.py
@@ -8,6 +8,7 @@ from utils.logger import logger
 
 CHUNK_SIZE = 10
 
+
 class DataPoller:
     def __init__(self,
                  macrotrends_api: StockDataAPI,
@@ -16,18 +17,25 @@ class DataPoller:
                  ratelimit_buffer: tuple[float] = (0.0, 1.0)) -> None:
         self.exchanges = exchanges
         self.base_ratelimit = base_ratelimit
-        self.ratelimit_buffer = ratelimit_buffer  # Adds unpredictability to request timing
+        self.ratelimit_buffer = ratelimit_buffer  # Used to add some unpredictability to the request timing
         self.api = macrotrends_api
         
     def do_sleep(self) -> None:
-        """Sleep for a random time between the base ratelimit and the buffer range."""
+        """
+        Sleep for a random time between the base ratelimit and the buffer range.
+        """
         time.sleep(self.base_ratelimit + random.uniform(*self.ratelimit_buffer))
         
     def poll_tickers(self) -> None:
-        """Poll all tickers for P/B from the exchanges and store them in the database."""
+        """
+        Poll all tickers for P/B from the exchanges and store them in the database.
+        """        
         
         # Get all tickers from the exchanges
         try:
+            logger.debug(f"Fetching exchange tickers ({self.exchanges})")
+        
+            # Fetch all tickers from the exchanges
             all_tickers_df = self.api.get_exchange_tickers(self.exchanges)
         except Exception as e:
             logger.error(f"Error occurred when fetching exchange tickers ({self.exchanges}): {e}")
@@ -39,6 +47,7 @@ class DataPoller:
                         password='sample_password', 
                         host='localhost',
                         port='5432') as database:
+            logger.debug(f"P/B & P/E Polling started for {len(all_tickers_df)} tickers. Est. time: ~{(len(all_tickers_df) * 20)/3600:.1f} hours.")
             
             ratio_history_df_chunk = []
             current_ratio_rows = []
@@ -46,26 +55,28 @@ class DataPoller:
                 # Get the P/B history
                 try:
                     pb_history_df, current_pb = self.api.get_pb_ratio_history(r['symbol'], r['full_name'], r['url'])
-                    if pb_history_df is None or current_pb is None:
-                        logger.warning(f"No P/B data returned for {r['symbol']}. Skipping...")
-                        continue
+
+                    logger.debug(f"({i+1}/{len(all_tickers_df)}) Fetched {len(pb_history_df)} P/B history records for {r['symbol']} ({r['full_name']})")
                     
+                    # Sleep for the ratelimit
                     self.do_sleep()
                 except Exception as e:
                     logger.warning(f"Could not fetch P/B history for {r['url']}: {e}")
+                    # Skip this ticker because we need the P/B history df as a foundation
                     continue
                 
                 # Get the P/E history
                 try:
                     pe_history_df, current_pe = self.api.get_pe_ratio_history(r['symbol'], r['full_name'], r['url'])
-                    if pe_history_df is None or current_pe is None:
-                        logger.warning(f"No P/E data returned for {r['symbol']}. Skipping...")
-                        continue
                     
+                    logger.debug(f"({i+1}/{len(all_tickers_df)}) Fetched {len(pe_history_df)} P/E history records for {r['symbol']} ({r['full_name']})")
+                    
+                    # Sleep for the ratelimit
                     self.do_sleep()
                 except Exception as e:
                     logger.warning(f"Could not fetch P/E history for {r['url']}: {e}")
-                    continue
+                    # Sleep for the ratelimit
+                    self.do_sleep()
                     
                 # Concatenate the P/B and P/E history
                 try:
@@ -74,7 +85,6 @@ class DataPoller:
                     ratio_history_df_chunk.append(concat_df)
                 except Exception as e:
                     logger.warning(f"Could not concatenate P/B and P/E history for {r['symbol']} ({r['full_name']}): {e}")
-                    continue
                     
                 # Build the row for current P/B and P/E
                 current_ratio_rows.append({
@@ -86,20 +96,21 @@ class DataPoller:
                 
                 # Store the dataframes immediately
                 try:
-                    if concat_df:
-                        database.store_report_dataframes([concat_df])
+                    database.store_report_dataframes([concat_df])
+                    logger.debug(f"Stored historical ratio data for {r['symbol']} ({r['full_name']}).")
                 except Exception as e:
                     logger.warning(f"An error occurred when storing historical ratio data for {r['symbol']} ({r['full_name']}):", exc_info=True)
                 finally:
                     ratio_history_df_chunk.clear()                    
                     
                 try:
-                    if current_ratio_rows:
-                        database.store_current_ratio_dataframes([pd.DataFrame(current_ratio_rows)])
+                    database.store_current_ratio_dataframes([pd.DataFrame(current_ratio_rows)])
+                    logger.debug(f"Stored current ratio data for {r['symbol']} ({r['full_name']}).")
                 except Exception as e:
                     logger.warning(f"An error occurred when storing current ratio data for {r['symbol']} ({r['full_name']}):", exc_info=True)
                 finally:
                     current_ratio_rows.clear()
+                    
     
     def run(self) -> None:
         max_retries = 5  # Maximum number of retries
